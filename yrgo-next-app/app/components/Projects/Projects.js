af5b03e9989021df4554c5f378a8ee6efe3f1969
Hannes Alexandersson <S103154@gmail.com>
2024-05-17 19:32:05 +0200

    Create Projects.js

---
+++ yrgo-next-app/app/components/Projects/Projects.js
@@ -0,0 +1,15 @@
+
+
+export default function Projects() {
+    return (
+        <div>
+            <ul>
+                <li>Protech</li>
+                <li>Retronautica</li>
+                <li>Branchevent</li>
+                <li>Waterworks</li>
+                <li>Yrgopelago</li>
+            </ul>
+        </div>
+    );
+  }
